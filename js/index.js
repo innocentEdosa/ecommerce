389067d5e7c046d60a6c0c44bf8f4dce1d3cf118
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2022-11-02 14:01:20 +0100

    first changes

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// As a user i should be able to add products to cart
+// As a user I should be able to see a list of products 
+// As a use I should be able to filter products
+// As a user I should see product details
+
+// As a user i should be able to create an account
+// As a user i should be able to login to the platform
